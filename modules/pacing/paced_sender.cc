482720e6ce15ddcaa2ad93aa9693a03caaef3187
philipel <philipel@webrtc.org>
2017-10-26 13:40:11 +0200

    New PacketQueue2 behind WebRTC-RoundRobinPacing field trial.
    To make testing easier all of PacketQueues functions have been made virtual,
    and PacketQueue2 now inherits PacketQueue. This change was made to minimize
    changes in PacedSender.
    
    (cherry picked from commit ccdfccaa6f86a6f301c2e722bea65aead15ec170)
    
    Bug: webrtc:8287, webrtc:8288
    Change-Id: I2593340e7cc7da617370b0a33e7b9deeb46d9487
    Reviewed-on: https://webrtc-review.googlesource.com/9380
    Reviewed-by: Stefan Holmer <stefan@webrtc.org>
    Commit-Queue: Philip Eliasson <philipel@webrtc.org>
    Cr-Original-Commit-Position: refs/heads/master@{#20385}
    Reviewed-on: https://webrtc-review.googlesource.com/15102
    Cr-Commit-Position: refs/branch-heads/63@{#9}
    Cr-Branched-From: bef8a5d2ca5413c680995584b8c0976852ba5f25-refs/heads/master@{#20237}

--- modules/pacing/paced_sender.cc
+++ modules/pacing/paced_sender.cc
@@ -59,7 +59,9 @@ PacedSender::PacedSender(const Clock* clock,
       pacing_bitrate_kbps_(0),
       time_last_update_us_(clock->TimeInMicroseconds()),
       first_sent_packet_ms_(-1),
-      packets_(new PacketQueue(clock)),
+      packets_(webrtc::field_trial::IsEnabled("WebRTC-RoundRobinPacing")
+                   ? new PacketQueue2(clock)
+                   : new PacketQueue(clock)),
       packet_counter_(0),
       pacing_factor_(kDefaultPaceMultiplier),
       queue_time_limit(kMaxQueueLengthMs) {
