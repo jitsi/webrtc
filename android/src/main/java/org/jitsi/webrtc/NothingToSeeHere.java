74ad273462d98087fa9f93ea18f97150f4778e3a
Saúl Ibarra Corretgé <s@saghul.net>
2023-03-14 20:45:41 +0100

    Add Android files

---
+++ android/src/main/java/org/jitsi/webrtc/NothingToSeeHere.java
@@ -0,0 +1,7 @@
+package org.jitsi.webrtc;
+
+/**
+ * Empty class, just so that this Java library is not empty.
+ * This library only has the purpose of packaging the WebRTC build.
+ */
+public class NothingToSeeHere { }
