25be2fa0b1683d9313fdf924ee8fa4f9ca0abfba
Saúl Ibarra Corretgé <s@saghul.net>
2025-01-07 13:34:03 +0100

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,9 +1,9 @@
-// swift-tools-version:5.6
+// swift-tools-version:5.7
 import PackageDescription
 
 let package = Package(
     name: "JitsiWebRTC",
-    platforms: [.iOS(.v12)],
+    platforms: [.iOS(.v12), .macOS(.v13)],
     products: [
         .library(
             name: "WebRTC",
@@ -13,7 +13,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "WebRTC",
-            url: "https://github.com/jitsi/webrtc/releases/download/v124.0.1/WebRTC.xcframework.zip",
+            url: "https://github.com/jitsi/webrtc/releases/download/v124.0.2/WebRTC.xcframework.zip",
             checksum: "d2548ea930ee135ea8269041c34031f388014d2b2c8aa5f7a7cfb892b603c31c"
         ),
     ]
